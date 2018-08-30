436ff2cdddff310ab44985cb3f9d37be49ba45ca
jasonmhoule <jasonmhoule@gmail.com>
2018-08-29 21:55:33 -0500

    Update keeper league scoring settings

--- scrapeValues.R
+++ scrapeValues.R
@@ -106,26 +106,26 @@ league_scoring <- function(league) {
     scoring_settings <- list(
       pass = list(
         pass_att = 0, pass_comp = 0, pass_inc = 0, pass_yds = 0.04, pass_tds = 4,
-        pass_int = -3, pass_40_yds = 0,  pass_300_yds = 0, pass_350_yds = 0,
-        pass_400_yds = 0
+        pass_int = -2, pass_40_yds = 0,  pass_300_yds = 0, pass_350_yds = 0,
+        pass_400_yds = 5
       ),
       rush = list(
         all_pos = TRUE,
         rush_yds = 0.1,  rush_att = 0, rush_40_yds = 0, rush_tds = 6,
-        rush_100_yds = 0, rush_150_yds = 0, rush_200_yds = 0),
+        rush_100_yds = 0, rush_150_yds = 0, rush_200_yds = 5),
       rec = list(
         all_pos = TRUE,
-        rec = 0, rec_yds = 0.1, rec_tds = 6, rec_40_yds = 0, rec_100_yds = 0,
-        rec_150_yds = 0, rec_200_yds = 0
+        rec = 0, rec_yds = 0.1, rec_tds = 6, rec_40_yds = 0, rec_100_yds = 2.5,
+        rec_150_yds = 0, rec_200_yds = 2.5
       ),
       misc = list(
         all_pos = TRUE,
-        fumbles_lost = -3, fumbles_total = 0,
+        fumbles_lost = -1, fumbles_total = -1,
         sacks = 0, two_pts = 2
       ),
       kick = list(
         xp = 1.0, fg_0019 = 3.0,  fg_2029 = 3.0, fg_3039 = 3.0, fg_4049 = 4.0,
-        fg_50 = 5.0,  fg_miss = 0.0
+        fg_50 = 5.0,  fg_miss = -1
       ),
       ret = list(
         all_pos = TRUE,
@@ -138,14 +138,17 @@ league_scoring <- function(league) {
       ),
       dst = list(
         dst_fum_rec = 2,  dst_int = 2, dst_safety = 2, dst_sacks = 1, dst_td = 6,
-        dst_blk = 1.5, dst_ret_yds = 0, dst_pts_allowed = 0
+        dst_blk = 2, dst_ret_yds = 0, dst_pts_allowed = 0
       ),
       pts_bracket = list(
-        list(threshold = 0, points = 10),
-        list(threshold = 6, points = 7),
-        list(threshold = 20, points = 4),
-        list(threshold = 34, points = 0),
-        list(threshold = 99, points = -4)
+        list(threshold = 0, points = 5),
+        list(threshold = 6, points = 4),
+        list(threshold = 13, points = 3),
+        list(threshold = 17, points = 1),
+        list(threshold = 27, points = 0),
+        list(threshold = 34, points = -1),
+        list(threshold = 45, points = -3),
+        list(threshold = 99, points = -5)
       )
     ) }
   else {
